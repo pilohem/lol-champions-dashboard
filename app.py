413ef19922914b624197210109db63902547a65f
pilohem <pilohem@gmail.com>
2021-09-22 09:36:41 -0500

    Add champion type feature and update gitignore file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+from dash_bootstrap_components._components.Col import Col
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
@@ -13,33 +14,36 @@ data = json.load(f)
 
 available_champions = list(data['data'].keys())
 champions_stats = utils.get_all_champion_stats(data, available_champions)
+champion_types = ['Assassin', 'Fighter', 'Mage', 'Marksman', 'Support', 'Tank']
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
-                external_stylesheets=[dbc.themes.BOOTSTRAP])
+                external_stylesheets=[dbc.themes.DARKLY])
 
 PAGE_SIZE = 5
 
 app.layout = html.Div(children=[
     html.Div(children=[
         html.H2('League of Legends Champions'),
-        dcc.Dropdown(id='champ-select', options=[{'label': champ, 'value': champ} for champ in available_champions], value='Ahri'),
+        dcc.Dropdown(id='champ-select', options=[{'label': champ_type, 'value': champ_type} for champ_type in champion_types], value='Mage'),
         html.P(id='champ-info'),
         dash_table.DataTable(
             id='champions-table',
             columns=[
                 {"name": col, "id": col, 'presentation': 'markdown'} for col in champions_stats.columns
             ],
+            style_header={'backgroundColor': 'rgb(30, 30, 30)'},
+            style_cell={
+            'backgroundColor': 'rgb(50, 50, 50)',
+            'color': 'white'
+            },
             page_current=0,
             page_size=PAGE_SIZE,
             page_action='custom',
-            markdown_options={'link_target': '_self'}
-        ),
+            markdown_options={'link_target': '_self'}),
         dcc.Location(id='location'),
-        html.H4(
-            id='selected-champion'
-        )
-        
-    ])
+        dbc.Row(id='selected-champion'),
+        dbc.Row(id='selected-champion-type')
+    ], style={'margin': '2rem 8rem'})
 ])
 
 @app.callback(
@@ -56,7 +60,37 @@ def update_table(page_current, page_size):
     Input('location', 'pathname')
 )
 def update_selected_champion(pathname):
-    return app.strip_relative_path(pathname)
+    champion = app.strip_relative_path(pathname)
+    card1 = dbc.Card(
+            [
+                dbc.CardImg(src=f"assets/img/champion/{champion}_0.jpg", top=True),
+            ],
+    style={"width": "10rem"}
+    )
+    card2 = dbc.CardBody(
+            [
+                html.H4(f"{champion}", className="card-title"),
+                html.P(data['data'][champion]['title']),
+                html.P(
+                    data['data'][champion]['blurb'],
+                    className="card-text",
+                ),
+                dbc.Button("Go somewhere", color="primary"),
+            ]
+        )
+    cards = dbc.Row([dbc.Col(card1, width='auto'), dbc.Col(card2)])
+
+    return cards
+
+@app.callback(
+    Output('selected-champion-type', 'children'),
+    Input('champ-select', 'value')
+)
+def update_selected_champions_by_type(selected_type):
+    champions_tags = utils.get_champions_with_tags(data)
+    return html.H4(utils.get_champions_by_type(selected_type, champions_tags)[selected_type])
+    
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- utils.py
+++ utils.py
@@ -17,3 +17,21 @@ def get_all_champion_stats(data, champions_list):
         stats = get_champion_stats(data, champion)
         stats_all = pd.concat([stats_all, stats], ignore_index=True)
     return stats_all
+
+def get_champions_with_tags(data):
+    """Get all champions grouped by champion type"""
+    champions_list = list(data['data'].keys())
+    champions_tags = {}
+    for champion in champions_list:
+        tags = data['data'][champion]['tags']
+        champions_tags[champion] = tags
+    return champions_tags
+
+def get_champions_by_type(champion_type, champions_tags):
+    """Get champions from a single champion type"""
+    champions_list = list(champions_tags.keys())
+    champions_type_list = []
+    for champion in champions_list:
+        if champion_type in champions_tags[champion]:
+            champions_type_list.append(champion)
+    return {champion_type: champions_type_list}
